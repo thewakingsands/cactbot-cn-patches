239cf9a40b46cb51d570f457a9606a7e0c9d7708
oott123 <git@public.oott123.com>
2020-09-13 15:30:22 +0800

    chore: update patch args

--- bin/patch.sh
+++ bin/patch.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 for patch in patches/*.diff
 do
   echo "Applying patch $patch ..."
-  cat "$patch" | git -C cactbot apply
+  cat "$patch" | git -C cactbot apply --ignore-space-change --ignore-whitespace
 done
 echo "Patches applied!"
