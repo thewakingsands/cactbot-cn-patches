270d7463a309027dc319bd5a2196cf956ed1b4fa
oott123 <git@public.oott123.com>
2021-08-08 14:43:13 +0800

    update

--- bin/pack.sh
+++ bin/pack.sh
@@ -5,4 +5,21 @@ cd cactbot
 npm install
 npm run build
 cd ..
-7z a releases/cactbot-cn-"$UPSTREAM_TAG"-web-only.zip cactbot/LICENSE cactbot/ui cactbot/user cactbot/resources cactbot/dist
+
+OUT=cactbot_webonly
+BASE=cactbot
+
+rm -rf $OUT
+mkdir -p $OUT
+
+cp -r $BASE/dist/ui/ $OUT/
+cp -r $BASE/dist/resources/ $OUT/
+cp $BASE/*.md $OUT/
+
+# manually copy readme due to avoid local user/ files
+mkdir -p $OUT/user/
+cp $BASE/user/README.txt $OUT/user/
+cp $BASE/user/*-example.* $OUT/user/
+cp $BASE/user/test-override.txt $OUT/user/
+
+7z a releases/cactbot-cn-`date "+%Y-%m-%d-%H-%M-%S"`-web-only.zip $OUT
